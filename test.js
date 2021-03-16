e2c3457532da85d57c85eb24db6f2155b888c074
klas9802 <klas9806@gmail.com>
2021-03-16 15:24:09 +0800

    更改test 新增html

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
-console.log("a")
-console.log("b")
+var canvas = document.getElementById("game");
+var context = canvas.getContext('2d');
+context.fillStyle="red";
+context.fillRect(0, 0, 200, 200);
+
+var ul = document.querySelector(".items");
+ul.children[0].textContent="hello";
+ul.children[1].textContent="world";
+console.log(ul)
+
