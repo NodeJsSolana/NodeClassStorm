12b59d905b3660f3b712d4ca0a85c4d9a9978e01
klas9802 <klas9806@gmail.com>
2021-05-11 15:52:56 +0800

    ng for* ok

---
+++ V511_branch/my-app/src/app/my-products.ts
@@ -0,0 +1,20 @@
+import {Product} from './product'
+
+
+export const PRODUCTS: Product[] = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone withone of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
