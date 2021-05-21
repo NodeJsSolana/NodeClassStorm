4f6ca57b65406eacefdd3935e92c9b8ef08d1563
klas9802 <klas9806@gmail.com>
2021-05-21 14:08:59 +0800

    all ok

--- V518_branch/my-app/src/app/parks/park-list/park-list.component.ts
+++ V518_branch/my-app/src/app/parks/park-list/park-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Park,PARKS } from "../park";
+import { ParkDataService } from '../park-data.service';
 
 @Component({
   selector: 'app-park-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParkListComponent implements OnInit {
 
-  constructor() { }
+  id : number = 0;;
+  parks:Park[]=[];
+  thePark?:Park;
+  searchString:string='';
+
+  constructor(private parkDataServ:ParkDataService) {
+    //this.parks=PARKS;
+  }
 
   ngOnInit(): void {
+    this.parkDataServ.getPark(this.id).then(data=>this.thePark=data);
+  }
+
+  getParks():void{
+    this.parkDataServ.getParks().then(data => this.parks = data);
+    if(this.searchString && this.searchString.trim()!=''){
+      this.parkDataServ.getFilteredParks(this.searchString).then(parks => {
+        this.parks = parks;
+      })
+    }
   }
 
 }
--- V518_branch/my-app/src/app/parks/parks-routing.module.ts
+++ V518_branch/my-app/src/app/parks/parks-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ParkDetailComponent} from './park-detail/park-detail.component';
+import {ParkListComponent} from './park-list/park-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'parks', component: ParkListComponent},
+  {path:'park/:id', component:ParkDetailComponent},
+  {path:'', redirectTo:"/parks", pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
